e7b75f905524aba5bc500a5e444ad6ac2795ad71
Nate Clark <natec425@gmail.com>
2017-10-30 13:20:50 -0500

    Add initial markup and PAGE_DATA blob

---
+++ index.js
@@ -0,0 +1,93 @@
+var PAGE_DATA = {
+    user: {
+        name: 'Raymond Hettinger',
+        username: 'raymondh',
+        description:
+            'Python core developer. Freelance programmer/consultant/trainer. Husband to Rachel. Father to Matthew.',
+        location: 'Santa Clara, CA',
+        website: 'rhettinger.wordpress.com',
+        joined: {
+            month: 3,
+            year: 2008
+        }
+    },
+    chips: [
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 10,
+                day: 28,
+                year: 2017
+            },
+            message:
+                '#python tip:  iter(C, sentinel) returns an iterator that invokes the callable C until it returns a sentinel signaling the iterator is done.'
+        },
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 10,
+                day: 25,
+                year: 2017
+            },
+            message:
+                '#python teaching tip:  When teaching adults, half of your time should be spent helping students unlearn pre-existing incorrect knowledge.'
+        },
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 10,
+                day: 23,
+                year: 2017
+            },
+            message:
+                '#python insight of the day:  Directories are a namespace and behave like dictionaries where the key is a filename and the value is an inode.'
+        },
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 10,
+                day: 5,
+                year: 2017
+            },
+            message: '#python news:  #PyPy version 5.9 has just been released.'
+        },
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 9,
+                day: 24,
+                year: 2017
+            },
+            message:
+                'Put another way. With "yield" the consumer controls execution. With "await" the producer controls execution. Very different points of view.'
+        },
+        {
+            author: {
+                name: 'Raymond Hettinger',
+                username: 'raymondh'
+            },
+            date: {
+                month: 9,
+                day: 24,
+                year: 2017
+            },
+            message:
+                'With #python iterators, we think of next() as initiating execution. With coroutines, we "await" a downstream event to initiate execution.'
+        }
+    ]
+};
